2c2966178b704b7bab9161329c33e20744779bb8
A.Taylor <ashtrix2k@googlemail.com>
2014-01-29 23:24:57 +0000

    Added Py_Calculate.py file to project
    Source file

---
+++ Projects/Calculator Project/Py_Calculate.py
@@ -0,0 +1,20 @@
+"""
+- Project : Py Calculate
+- Project description : A simple calculator program.
+- Language : Python
+- Coded by : A.Taylor
+- Coder Name : S3pHiRoTh
+"""
+
+#	Import python sys module
+import sys
+
+#	Location of python src folder
+# 'Src' is a valid path since this file will be located in the root directory of the project folder
+sys.path.insert(0, "Src")
+
+#	Import Py Calculate source file
+from Py_Calculate_Main import runapp
+
+if __name__ in "__main__" : 
+	j = runapp()
